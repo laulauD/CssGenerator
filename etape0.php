98e1bea637a2de595a683829433e55c488aa9787
laulauD <laulaudewaele@gmail.com>
2020-12-03 17:43:26 +0100

    Bootstrap 1.0

--- etape0.php
+++ etape0.php
@@ -1,27 +1,61 @@
 <?php
-function my_merge_image($first_img_path, $second_img_path){
-    $width = 800;
-    $height = 600;
+//etape 0 et 1
+function my_merge_image($first, $second, $filename){
 
-    header("Content-Type: image/png");
-    $sprite = imagecreatetruecolor($width, $height);
+  list($img1_width, $img1_height) = getimagesize($first);
+  list($img2_width, $img2_height) = getimagesize($second);
 
-    $image_a = imagecreatefrompng($first_img_path);
-    imagecopyresampled($sprite, $image_a, 0, 0, 0, 0, 500, 206, 600, 500);
 
-    $image_b = imagecreatefrompng($second_img_path);
-    imagecopyresampled($sprite, $image_b, 408, 0, 0, 0, 500, 206, 600, 500);
+  $source1 = imagecreatefrompng($first);
+  $source2 = imagecreatefrompng($second);
 
-    // OTHERS IMAGES
-    // $image_c = imagecreatefrompng($);
-    // imagecopyresampled($sprite, $image_c, 0, 200, 0, 0, 500, 206, 600, 500);
+  $new_width = $img1_width > $img2_width ? $img1_width : $img2_width;
+  $new_height = $img1_height + $img2_height;
+  $new = imagecreatetruecolor($new_width, $new_height);
 
-    // $image_d = imagecreatefrompng($);
-    // imagecopyresampled($sprite, $image_d, 408, 200, 0, 0, 500, 206, 600, 500);
+  imagealphablending($new, false);
+  imagesavealpha($new, true);
 
-    imagepng($sprite, "merge.png");
-    imagepng($sprite);
+  imagecopy($new, $source1, 0, 0, 0, 0, $img1_width, $img1_height);
+  imagecopy($new, $source2, 0, $img1_height, 0, 0, $img2_width, $img2_height);
+  imagepng($new, "$filename");
 
+  $fp = fopen("style.css", 'w');
+  fwrite($fp, ".my_sprite {
+    background-image: url(".$filename.");
+    background-repeat: no-repeat;
+    display: block;
+    background-color:pink;
+  }\n");
+
+  fwrite($fp, "#img1 {
+    width: ".$img1_width."px;
+    height: ".$img1_height."px;
+    background-position: 0px 0px;
+  }\n");
+
+  fwrite($fp, "#img2 {
+    width: ".$img2_width."px;
+    height: ".$img2_height."px;
+    background-position: 0px -".$img1_height."px;
+  }");
+  fclose($fp);
 }
-my_merge_image("a.png", "b.png");
-?>
+//etape2
+function my_scandir($dir_path){
+
+  if ($handle = opendir('.')) {
+
+   while (false !== ($entry = readdir($handle))) {
+
+       if (substr($entry, -3) == "png") {
+           echo "$entry\n";
+       }
+   }
+   closedir($handle);
+}
+}
+
+my_merge_image($argv[1], $argv[2], "laurine.png");
+my_scandir("CssGenerator");
+ ?>
